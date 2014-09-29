5c742ae18c496226ac4c3766526d65c96953c6d6
Dave Nelson <davidicus21@gmail.com>
2014-09-29 15:38:57 -0600

    busey test passes

--- app.js
+++ app.js
@@ -3,16 +3,27 @@
 var fs = require('fs')
   , express = require('express')
   , app = express()
-  , url = require('url');
+  , url = require('url')
+  , gm = require('gm');
 
 app.get('/', function  (req, res) {
   var urlParts = url.parse(req.url, true);
   var query = urlParts.query;
-  console.log(query.w);
-  console.log(query.h);
-  console.log(query.url)
   console.log(query);
-});
+  gm(query.url)
+    .resize(query.w, query.h)
+    .options({imageMagick: true})
+    .noProfile()
+    .write('busey.png', function (err) {
+      if (!err) {
+        console.log('done');
+      } else{
+        console.log(err);
+      }
+    });
+  }
+);
 
 app.listen(3000);
 console.log("Listening on 3000");
+// localhost:3000?w=200&h=200&url=http://upload.wikimedia.org/wikipedia/commons/4/4f/Gary_Busey_2007.jpg
